34ff0424cfe1e7f688a4373d015b8e45db027eb2
brenrecorder <132142660+brenrecorder@users.noreply.github.com>
2023-05-24 17:20:04 +0200

    Update searchengine.php

--- searchengine.php
+++ searchengine.php
@@ -1,7 +1,7 @@
 <?php
-	header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: GET,PUT,POST,DELETE,PATCH,OPTIONS');
-	header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET,PUT,POST,DELETE,PATCH,OPTIONS');
+header('Content-Type: application/json');
 $sqlNewTableURL = "CREATE TABLE Links (
 id INTEGER PRIMARY KEY,
 PageURL TEXT NOT NULL,
@@ -15,12 +15,8 @@ if (file_exists("urldb.db")) { //CREATE DATABASE
 } else {
 	$db = new SQLite3('urldb.db');
 	$db->querySingle($sqlNewTableURL);
-
-	
-	//$db->exec("INSERT INTO CoinSecure(Address, Amount, AmountRGB) VALUES('TotalMarket', '0', 'XXXXXX')");	
 	echo "Tables and file created..";
 }
-
 	if (!empty($_GET['action'])) { $action = $_GET['action']; } else { $action = ""; }
 	if ($action == 'addpage') {
 	if (!empty($_GET['url'])) { $url = $_GET['url']; } else { $url = ""; }
@@ -29,9 +25,8 @@ if (file_exists("urldb.db")) { //CREATE DATABASE
 	$idexists = $db->querySingle("SELECT COUNT(id) AS count FROM Links WHERE PageURL=='".$url."'");
 	if ($idexists < 1) {
 	$db->exec("INSERT INTO Links(PageURL, PageTitle, PageDescription) VALUES('".$url."', '".$title."', '".$desc."')");
-		$response = array("response"=>"added", "url"=>$url);	} else {
-	$response = array("response"=>"failed", "url"=>$url);
-		}			
+	$response = array("response"=>"added", "url"=>$url); } else {
+	$response = array("response"=>"failed", "url"=>$url); }			
 	echo json_encode($response);
 	}
 	if ($action == 'pagesjson') {
@@ -42,15 +37,13 @@ if (file_exists("urldb.db")) { //CREATE DATABASE
 		$arrlinks = array();
 		while($row= $result->fetchArray()){
 			
-if (substr($row['PageURL'], 0, 7) != 'http://' && substr($row['PageURL'], 0, 8) != 'https://'){
-$urlout = "http://".$row['PageURL'];
-}else {
-$urlout = $row['PageURL'];	
-}
-
+	if (substr($row['PageURL'], 0, 7) != 'http://' && substr($row['PageURL'], 0, 8) != 'https://'){
+	$urlout = "http://".$row['PageURL'];
+	}else {
+	$urlout = $row['PageURL'];	
+	}
 		array_push($arrlinks, array("title"=>$row['PageTitle'], "desc"=>$row['PageDescription'], "url"=>$urlout));
 	}
 	echo json_encode($arrlinks);
 	}
-	
-	
+?>
